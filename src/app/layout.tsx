9a20120bab7e82189e17e6d4bdea411c8c42f612
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-08-21 22:23:08 +0400

    refactor: add directory 'assets'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import localFont from 'next/font/local'
-import "./globals.scss";
+import './globals.scss';
 
 const ceraPro = localFont({
   src: [
     {
-      path: './../fonts/CeraPro-Regular.woff2',
+      path: './../assets/fonts/CeraPro-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './../fonts/CeraPro-Bold.woff2',
+      path: './../assets/fonts/CeraPro-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
