ac3ecc1028c755a61a6d58caaadb07b2fa33822a
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-08-21 19:18:54 +0400

    chore: add sass in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+			GraphiQL
     </main>
   );
 }
