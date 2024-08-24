1da562b5b36d436a36e74cada96aa8a4a772ba10
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-08-24 10:43:09 +0300

    feat: RSS-GRAPHI-1_12 Create the Footer component (#22)
    * feat: RSS-GRAPHI-1_12 Create the Footer component
    
    * feat: RSS-GRAPHI-1_12 add link to rsschool
    
    * feat: RSS-GRAPHI-1_12 implement adaptive layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import rssLogo from '../../assets/images/rss-logo.svg';
+import style from './Footer.module.scss';
+
+function Footer(): JSX.Element {
+  return (
+    <footer className={style.footer}>
+      <div className={style.developers}>
+        <Link href='https://github.com/Wood85' target='_blank' className={style.developer}>
+          Wood85
+        </Link>
+        <Link href='https://github.com/doosterhere' target='_blank' className={style.developer}>
+          doosterhere
+        </Link>
+        <Link href='https://github.com/kagerka' target='_blank' className={style.developer}>
+          kagerka
+        </Link>
+      </div>
+
+      <div className={style.year}>2024</div>
+
+      <Link href='https://rs.school/courses/reactjs' target='_blank' className={style.rss_link}>
+        <Image src={rssLogo} className={style.rss_logo} alt='RSSchool' priority />
+      </Link>
+    </footer>
+  );
+}
+export default Footer;
+
