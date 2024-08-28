9b3ead4415359f73c8d449b8972a86e92fa13797
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-28 16:51:25 +0300

    feat: RSS-GRAPHI-2_01 complete About component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -29,16 +29,16 @@ const TEAM = [
 
 function About(): JSX.Element {
   return (
-    <section className={style.about}>
+    <section className={style.wrapper}>
       <h2 className={style.title}>About us</h2>
-      <p className={style.paragraph}>
+      <p className={style.description} lang='en-US'>
         This application is a result of our work as a team while studying at RS School, React 2024 Q3 course. We created
-        a light-weight versions of Postman and GrqphiQL, combined in one app.
+        a light-weight versions of Postman and GraphiQL, combined in one app.
       </p>
       <div className={style.cards}>
         {TEAM.map(({ name, teamLead, frontDev, image }) => (
           <div className={style.card} key={name}>
-            <Image src={image} width={300} height={300} alt={name} priority />
+            <Image src={image} width={300} height={300} alt={name} priority className={style.photo} />
             <span className={style.name}>{name}</span>
             <div className={style.roles}>
               {teamLead && <span className={style.role}>Team-lead</span>}
