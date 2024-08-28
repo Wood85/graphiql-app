b6b2f766c913ab9a9ea02eb55932e3096aba192f
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-08-28 17:39:32 +0300

    feat: RSS-GRAPHI-2_01 complete Welcome component

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,18 +1,58 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+
+import clsx from 'clsx';
+
+import SignInIcon from '@/assets/images/icons/SignInIcon';
+import SignUpIcon from '@/assets/images/icons/SignUpIcon';
+import Button from '../UI/Button/Button';
 
 import style from './Welcome.module.scss';
 
 function Welcome(): JSX.Element {
+  const [isAuth, setIsAuth] = useState(false);
+  const USERNAME = 'User';
+
+  const toggelAuth = (): void => {
+    setIsAuth(!isAuth);
+  };
+
   return (
-    <section className={style.welcome}>
-      <h2 className={style.title}>Welcome!</h2>
-      <div className={style.buttons}>
-        <Link href='/sign-in' className={style.button}>
-          Sign In
-        </Link>
-        <Link href='/sign-up' className={style.button}>
-          Sign Up
-        </Link>
+    <section className={style.wrapper}>
+      <Button onClick={toggelAuth} className={clsx(style.button, style.absolute)}>
+        Temporary auth toggler
+      </Button>
+      <h2 className={style.title}>{isAuth ? `Welcome back, ${USERNAME}!` : 'Welcome!'}</h2>
+      <div className={style.actions}>
+        {!isAuth && (
+          <>
+            <Button href='/sign-in' className={style.button}>
+              {/* <Image src={signInIcon} alt='Sign In' width={24} height={24} priority /> */}
+              <SignInIcon className={style.sign_in_icon} />
+              Sign In
+            </Button>
+            <Button href='/sign-up' className={style.button}>
+              {/* <Image src={signUpIcon} alt='Sign Up' width={24} height={24} priority /> */}
+              <SignUpIcon className={style.sign_up_icon} />
+              Sign Up
+            </Button>
+          </>
+        )}
+        {isAuth && (
+          <>
+            <Link href='/restapi' className={style.link}>
+              REST Client
+            </Link>
+            <Link href='/graphiql' className={style.link}>
+              GraphiQL Client
+            </Link>
+            <Link href='/history' className={style.link}>
+              History
+            </Link>
+          </>
+        )}
       </div>
     </section>
   );
