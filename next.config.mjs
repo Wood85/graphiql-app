485ab6e6eda712559e5762f13a2f3baed2c949e1
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-02 20:06:32 +0300

    chore: configure redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/restapi/([A-Z]+)/:path*',
+        destination: '/restapi',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
