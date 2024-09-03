2141fb6d7f6ea22083367ad05f7c45edc2afe511
Mikalai Shcharbakou <69976654+doosterhere@users.noreply.github.com>
2024-09-03 20:45:24 +0300

    feat: RSS-GRAPHI-3_07 Implement style for the restapi page (#56)
    * feat: RSS-GRAPHI-3_07 add image components
    
    * style: RSS-GRAPHI-3_07 remove inappropriate elements
    
    * feat: RSS-GRAPHI-3_07 add the ROUTES constant
    
    * feat: RSS-GRAPHI-3_07 create the ClientTop component
    
    * style: RSS-GRAPHI-3_07 implement style for the restapi page
    
    * style: RSS-GRAPHI-3_07 fix gap

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,11 @@ export const PASSWORD_LENGTH = 8;
 export const EMPTY_ARR_LENGTH = 0;
 export const EMAIL_REGEXP =
   /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+export const ROUTES = {
+  HOME: '/',
+  RESTAPI: '/restapi',
+  GRAPHQL: '/graphql',
+  HISTORY: '/history',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+};
