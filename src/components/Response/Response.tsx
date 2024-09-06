c877996bf461919b0db55e8fddc0d9eee7f8d7c3
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-06 17:58:49 +0300

    feat: RSS-GRAPHI-3_03 Implement pretty/raw output
    BREAKING CHANGE:
    1. Add buttons to toggle between pretty and raw response data;
    2. Use @monaco-editor to output text/JSON/HTML/css;
    3. Use textarea to output raw data;
    4. Stylize the component;
    5. Use usePrepareOutput hook and remove all logic that has been moved to
       it

--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -1,11 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 
+import Editor from '@monaco-editor/react';
 import clsx from 'clsx';
 
-import type { IImageBody, IResponse, ITextBody } from '@/interfaces/Response';
-import { TRequestMethod } from '@/interfaces/RequestMethod';
-import { createResponseStatus } from '@/utils/createResponseStatus';
+import type { IResponse } from '@/interfaces/Response';
+import type { TRequestMethod } from '@/interfaces/RequestMethod';
+import usePrepareOutput from '@/hooks/usePrepareOutput';
+import Button from '../UI/Button/Button';
 
 import style from './Response.module.scss';
 
@@ -15,77 +17,65 @@ interface IProps {
 }
 
 function Response({ response, method }: IProps): JSX.Element {
-  const INDENT = 2;
-  const [statusString, setStatusString] = useState<{
-    value: string;
-    color: string;
-  } | null>(null);
-  const [output, setOutput] = useState<string>('');
-  const [outputType, setOutputType] = useState<'text' | 'image'>('text');
-  const [imageUrl, setImageUrl] = useState<string>('');
-  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
-
-  const prepareOutput = useCallback(() => {
-    if (response !== null && response?.body === null) {
-      setOutputType('text');
-      setOutput('');
-      return;
-    }
-
-    if (response !== null && response.headers['content-type']?.includes('json')) {
-      setOutputType('text');
-      setOutput(JSON.stringify(response.body, null, INDENT));
-      return;
-    }
-
-    if (
-      response !== null &&
-      (response.headers['content-type']?.includes('text') || response.headers['content-type']?.includes('html'))
-    ) {
-      setOutputType('text');
-      Boolean(response.body) && setOutput((response.body as ITextBody).text.trim());
-      return;
-    }
-
-    if (response !== null && response.headers['content-type']?.includes('image')) {
-      setOutputType('image');
-      const { url, width, height } = response.body as IImageBody;
-      Boolean(response.body) && setImageUrl(url);
-      setImageSize({ width, height });
-      return;
-    }
-
-    setOutputType('text');
-    setOutput('');
-  }, [response]);
-
-  useEffect(() => {
-    if (method === TRequestMethod.HEAD) {
-      const statusOK = 200;
-      setStatusString(createResponseStatus(statusOK, 'OK'));
-    }
-  }, [method]);
-
-  useEffect(() => {
-    if (response !== null) {
-      setStatusString(createResponseStatus(response.status, response.statusText));
-      prepareOutput();
-    }
-  }, [response, prepareOutput]);
+  const { output, outputType, outputLanguage, imageUrl, imageSize, statusString } = usePrepareOutput(response, method);
+  const [isPretty, setIsPretty] = useState(true);
 
   return (
     <div className={style.response}>
-      <div className={style.response_status_wrapper}>
-        <span className={clsx(style.response_status, style[statusString?.color ?? 'gray'])}>
-          {statusString?.value ?? ''}
-        </span>
-      </div>
-      {outputType === 'text' && <textarea className={style.response_body} value={output} readOnly />}
-      {outputType === 'image' && (
-        <div className={style.image_container}>
-          <Image src={imageUrl} alt='Image' width={imageSize.width} height={imageSize.height} />
+      <div className={style.response_controls}>
+        {outputType === 'text' && (
+          <div className={style.response_body_control}>
+            <Button
+              className={clsx(style.button, isPretty ? style.active : '')}
+              onClick={() => {
+                setIsPretty(true);
+              }}
+            >
+              Pretty
+            </Button>
+            <Button
+              className={clsx(style.button, isPretty ? '' : style.active)}
+              onClick={() => {
+                setIsPretty(false);
+              }}
+            >
+              Raw
+            </Button>
+          </div>
+        )}
+        <div className={style.response_status_wrapper}>
+          <span className={clsx(style.response_status, style[statusString?.color ?? 'gray'])}>
+            {statusString?.value ?? ''}
+          </span>
         </div>
-      )}
+      </div>
+      <div className={style.output_container}>
+        {outputType === 'text' && !isPretty && <textarea className={style.response_body_raw} value={output} readOnly />}
+        {outputType === 'text' && isPretty && (
+          <Editor
+            className={style.response_body_editor}
+            language={outputLanguage}
+            value={output}
+            options={{
+              fontFamily: '"Cera Pro"',
+              fontSize: 16,
+              minimap: { enabled: false },
+              padding: { top: 5, bottom: 5 },
+              readOnly: true,
+              renderLineHighlight: 'none',
+              scrollBeyondLastLine: false,
+              wordWrap: 'on',
+              wrappingIndent: 'deepIndent',
+              wrappingStrategy: 'advanced',
+            }}
+          />
+        )}
+        {outputType === 'image' && (
+          <div className={style.image_container}>
+            <Image src={imageUrl} alt='Image' width={imageSize.width} height={imageSize.height} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
