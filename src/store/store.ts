d6c3957f267dfe89914d6f7c5c6fe2ff489cfa64
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-09 15:44:38 +0300

    feat: RSS-GRAPHI-3_03 create loadingState slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import restFullReducer from '@/store/reducers/restFullSlice';
+import loadingStateReducer from '@/store/reducers/loadingStateSlice';
 
 export const store = configureStore({
   reducer: {
     rest: restFullReducer,
+    loadingState: loadingStateReducer,
   },
 });
 
