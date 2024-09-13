2045b974f0cb083e103a655fe37d3485cf7e5363
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-09-13 19:17:20 +0300

    fix: RSS-GRAPHI-5_03: Fix Tests

--- src/__tests__/About.test.tsx
+++ src/__tests__/About.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-
 import About from '@/app/[locale]/components/About/About';
+import { render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+import { describe, expect, it } from 'vitest';
+import messages from '../../messages/en.json';
 
+const locale = 'en';
 describe('About', () => {
   it('should render correctly', () => {
-    render(<About />);
+    render(
+      <NextIntlClientProvider messages={messages} locale={locale}>
+        <About />
+      </NextIntlClientProvider>,
+    );
 
     const title = screen.getByText('About us');
     const description = screen.getByText(/this application is a result of our work as a team/i);
--- src/app/[locale]/[...not_found]/page.tsx
+++ src/app/[locale]/[...not_found]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from 'next/navigation';
 
-export default function NotFoundCatchAll(): void {
+export default function NotFoundCatchAll(): JSX.Element {
   notFound();
 }
