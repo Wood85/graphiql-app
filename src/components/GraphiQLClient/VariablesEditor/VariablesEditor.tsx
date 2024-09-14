2a21fea60effb4d31223eac96cc2ef2987e12fd2
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-14 19:06:08 +0300

    feat: RSS-GRAPHI-4_04 implement VariablesEditor

--- src/components/GraphiQLClient/VariablesEditor/VariablesEditor.tsx
+++ src/components/GraphiQLClient/VariablesEditor/VariablesEditor.tsx
@@ -1,3 +1,12 @@
+import { useRef, useState } from 'react';
+
+import Editor from '@monaco-editor/react';
+import type { editor } from 'monaco-editor/esm/vs/editor/editor.api';
+
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { selectGraphQLVariables, setGraphQLVariables } from '@/store/reducers/graphQLVariablesSlice';
+import type { TGraphQLVars } from '@/store/reducers/graphQLVariablesSlice';
+
 import style from './VariablesEditor.module.scss';
 
 interface IProps {
@@ -6,15 +15,53 @@ interface IProps {
 }
 
 function VariablesEditor({ variables, setVariables }: IProps): JSX.Element {
+  const INDENT = 2;
+  const variablesObject = useAppSelector(selectGraphQLVariables);
+  const [variablesString, setVariablesString] = useState(JSON.stringify(variablesObject, null, INDENT));
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const dispatcher = useAppDispatch();
+
+  const onEditorMountHandler = async (editor: editor.IStandaloneCodeEditor): Promise<void> => {
+    editorRef.current = editor;
+  };
+
+  const onHandleEditorChange = (value: string | undefined): void => {
+    if (value !== undefined) {
+      try {
+        const parsedVariablesObject = JSON.parse(value);
+        dispatcher(setGraphQLVariables(parsedVariablesObject as TGraphQLVars));
+        setVariablesString(value);
+      } catch (e) {
+        console.log('error parsing json');
+      }
+    }
+  };
+
   return (
-    <textarea
-      className={style.editor}
-      placeholder='Query Editor'
-      value={variables}
-      onChange={(e) => {
-        setVariables(e.target.value);
-      }}
-    />
+    <div className={style.wrapper}>
+      <Editor
+        className={style.variables_editor}
+        language='json'
+        value={variablesString}
+        options={{
+          detectIndentation: false,
+          fontFamily: '"Cera Pro"',
+          fontSize: 16,
+          formatOnType: true,
+          formatOnPaste: true,
+          minimap: { enabled: false },
+          padding: { top: 5, bottom: 5 },
+          renderLineHighlight: 'none',
+          scrollBeyondLastLine: false,
+          tabSize: 2,
+          wordWrap: 'on',
+          wrappingIndent: 'deepIndent',
+          wrappingStrategy: 'advanced',
+        }}
+        onChange={onHandleEditorChange}
+        onMount={onEditorMountHandler}
+      />
+    </div>
   );
 }
 
