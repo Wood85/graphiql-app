3177955e5458773bab9669d1d7c0648d6a25a8d1
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-09-16 01:27:23 +0300

    feat: RSS-GRAPHI-6_01: add tests

---
+++ src/__tests__/Response.test.tsx
@@ -0,0 +1,33 @@
+import { Response, type IProps } from '@/components/Response/Response';
+import { TRequestMethod } from '@/interfaces/RequestMethod';
+import { store } from '@/store/store';
+import { render } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+import { Provider } from 'react-redux';
+import messages from '../../messages/en.json';
+
+const locale = 'en';
+describe('Response component', () => {
+  const props: IProps = {
+    method: TRequestMethod.GET,
+    response: {
+      body: 'Loading...',
+      status: 200,
+      statusText: 'OK',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  };
+
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Response {...props} />
+        </NextIntlClientProvider>
+      </Provider>,
+    );
+    expect(getByText('Loading...')).toBeDefined();
+  });
+});
