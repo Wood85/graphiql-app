7d48a224a6b70f34eaa3e5e135b49130d3dd4acc
Aleksandr Krivoshein <77492375+Wood85@users.noreply.github.com>
2024-09-15 15:57:40 +0400

    Feat: RSS-GRAPHI-4_05 Header Section (#84)
    * feat: implement graphql editor
    
    * fix: add 'use client' to query editor component
    
    * feat: RSS-GRAPHI-4_02 Implement response section
    
    * refactor: RSS-GRAPHI-4_02 remove comments
    
    * feat: RSS-GRAPHI-4_02 add loader
    
    * style: RSS-GRAPHI-4_02 style nesting
    
    * fix: RSS-GRAPHI-4_02 correct get of the currentRoute variable
    
    * fix: RSS-GRAPHI-4_02 correct apiUrl path
    
    * refactor: RSS-GRAPHI-4_02 remove console.log
    
    * fix: RSS-GRAPHI-4_02 request path for the HEAD method
    
    * fix: change the comment in the query editor
    
    * feat: add headers editor to the graphql client
    
    ---------
    
    Co-authored-by: Mikalai Shcharbakou <doosterhere@gmail.com>

--- src/components/RowEditor/RowEditor.tsx
+++ src/components/RowEditor/RowEditor.tsx
@@ -8,6 +8,7 @@ import {
   focusCellCurrentValue,
   variables,
 } from '@/store/reducers/restFullSlice';
+import { gqlHeaders, gqlFocusCellKey, gqlFocusCellValue } from '@/store/reducers/graphqlSlice';
 import Checkbox from '@/components/UI/Checkbox/Checkbox';
 import SaveIcon from '@/assets/images/icons/SaveIcon';
 import type TRows from '@/interfaces/Rows';
@@ -16,7 +17,7 @@ import styles from './RowEditor.module.scss';
 
 const START_POSITION = 0;
 
-type TTypeTable = 'headers' | 'variables';
+type TTypeTable = 'headers' | 'variables' | 'graphqlHeaders';
 interface IProps {
   type: TTypeTable;
 }
@@ -31,6 +32,10 @@ function RowEditor(props: IProps): JSX.Element {
   const variablesSelector = useAppSelector((state) => state.rest.variables);
   const focusVariableSelector = useAppSelector((state) => state.rest.focusCellVariable);
   const focusCurrentValueSelector = useAppSelector((state) => state.rest.focusCellCurrentValue);
+
+  const gqlHeadersSelector = useAppSelector((state) => state.graphql.headers);
+  const gqlFocusKeySelector = useAppSelector((state) => state.graphql.focusCellKey);
+  const gqlFocusValueSelector = useAppSelector((state) => state.graphql.focusCellValue);
 
   const dispatch = useAppDispatch();
 
@@ -73,6 +78,10 @@ function RowEditor(props: IProps): JSX.Element {
           if (type === 'variables') {
             dispatch(focusCellVariable(true));
             dispatch(focusCellCurrentValue(false));
+          }
+          if (type === 'graphqlHeaders') {
+            dispatch(gqlFocusCellKey(true));
+            dispatch(gqlFocusCellValue(false));
           }
           setAutofocus({ key: true, value: false });
         }}
@@ -140,6 +149,43 @@ function RowEditor(props: IProps): JSX.Element {
           />
         )}
         {type === 'variables' && !focusVariableSelector && keyInputValue}
+        {type === 'graphqlHeaders' && gqlFocusKeySelector && (
+          <>
+            <input
+              autoCapitalize='off'
+              className={styles.input}
+              type='text'
+              value={keyInputValue}
+              list='headers'
+              onChange={(e) => {
+                e.preventDefault();
+                setKeyInputValue(e.target.value);
+                if (e.target.selectionStart !== null) {
+                  setCursorPositionKey(e.target.selectionStart);
+                } else {
+                  setCursorPositionKey(START_POSITION);
+                }
+              }}
+              onFocus={(e) => {
+                e.preventDefault();
+                e.target.setSelectionRange(cursorPositionKey, cursorPositionKey);
+              }}
+              onKeyDown={onKeyDownInputHandler}
+              onBlur={() => {
+                setAutofocus({ key: false, value: false });
+              }}
+              autoFocus={autofocus.key}
+            />
+            <datalist id='headers' className={styles.datalist}>
+              {headersListSelector.map((item) => (
+                <option key={item} className={styles.option}>
+                  {item}
+                </option>
+              ))}
+            </datalist>
+          </>
+        )}
+        {type === 'graphqlHeaders' && !gqlFocusKeySelector && keyInputValue}
       </td>
       <td
         className={`${styles.td} ${styles.td_3}`}
@@ -153,6 +199,10 @@ function RowEditor(props: IProps): JSX.Element {
           if (type === 'variables') {
             dispatch(focusCellVariable(false));
             dispatch(focusCellCurrentValue(true));
+          }
+          if (type === 'graphqlHeaders') {
+            dispatch(gqlFocusCellKey(false));
+            dispatch(gqlFocusCellValue(true));
           }
           setAutofocus({ key: false, value: true });
         }}
@@ -209,6 +259,32 @@ function RowEditor(props: IProps): JSX.Element {
           />
         )}
         {type === 'variables' && !focusCurrentValueSelector && valueInputValue}
+        {type === 'graphqlHeaders' && gqlFocusValueSelector && (
+          <input
+            className={styles.input}
+            type='text'
+            value={valueInputValue}
+            onChange={(e) => {
+              e.preventDefault();
+              setValueInputValue(e.target.value);
+              if (e.target.selectionStart !== null) {
+                setCursorPositionValue(e.target.selectionStart);
+              } else {
+                setCursorPositionValue(START_POSITION);
+              }
+            }}
+            onFocus={(e) => {
+              e.preventDefault();
+              e.target.setSelectionRange(cursorPositionValue, cursorPositionValue);
+            }}
+            onKeyDown={onKeyDownInputHandler}
+            onBlur={() => {
+              setAutofocus({ key: false, value: false });
+            }}
+            autoFocus={autofocus.value}
+          />
+        )}
+        {type === 'graphqlHeaders' && !gqlFocusValueSelector && valueInputValue}
       </td>
       <td className={`${styles.td} ${styles.td_4}`} role='gridcell' aria-label='btns'>
         {keyInputValue !== '' && valueInputValue !== '' && (
@@ -247,6 +323,21 @@ function RowEditor(props: IProps): JSX.Element {
                 dispatch(focusCellVariable(false));
                 dispatch(focusCellCurrentValue(false));
                 dispatch(variables(copyVariables));
+              }
+              if (type === 'graphqlHeaders') {
+                const copyHeaders: TRows = JSON.parse(JSON.stringify(gqlHeadersSelector));
+                const newHeader: IRowWithCheckbox = {
+                  checked: isCurrentChecked,
+                  key: keyInputValue,
+                  value: valueInputValue,
+                  userDefined: true,
+                };
+                copyHeaders.push(newHeader);
+                setKeyInputValue('');
+                setValueInputValue('');
+                dispatch(gqlFocusCellKey(false));
+                dispatch(gqlFocusCellValue(false));
+                dispatch(gqlHeaders(copyHeaders));
               }
             }}
           >
