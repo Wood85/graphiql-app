848f10caa1f1e3ded7396271b4495a43de053163
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-09-16 02:43:45 +0300

    feat: RSS-GRAPHI-6_01: add tests

---
+++ src/__tests__/Table.test.tsx
@@ -0,0 +1,40 @@
+import { Table } from '@/components/Response/Table/Table';
+import { store } from '@/store/store';
+import { render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+import { Provider } from 'react-redux';
+import messages from '../../messages/en.json';
+
+const locale = 'en';
+
+describe('Table component', () => {
+  it('renders table with headers and rows', () => {
+    const headers = { key: 'Accept', value: '*/*' };
+    render(
+      <Provider store={store}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
+          <Table headers={headers} />
+        </NextIntlClientProvider>
+      </Provider>,
+    );
+
+    expect(screen.getByRole('table')).toBeDefined();
+    expect(screen.getByText('Accept')).toBeDefined();
+    expect(screen.getByText('*/*')).toBeDefined();
+  });
+
+  it('renders table with translated headers', () => {
+    const headers = { key: 'Accept', value: '*/*' };
+
+    render(
+      <Provider store={store}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
+          <Table headers={headers} />
+        </NextIntlClientProvider>
+      </Provider>,
+    );
+
+    expect(screen.getByText('key')).toBeDefined();
+    expect(screen.getByText('value')).toBeDefined();
+  });
+});
