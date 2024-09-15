6fb755e65f40142aedf3427dd475f604c1a2142a
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-09-16 02:29:59 +0300

    feat: RSS-GRAPHI-6_01: add tests

--- src/__tests__/RowEditor.test.tsx
+++ src/__tests__/RowEditor.test.tsx
@@ -1,12 +1,12 @@
 import RowEditor from '@/components/RowEditor/RowEditor';
 import { headers } from '@/store/reducers/restFullSlice';
-import { render, screen, act } from '@testing-library/react';
-import { NextIntlClientProvider } from 'next-intl';
-import { describe, expect, it } from 'vitest';
-import { Provider } from 'react-redux';
 import { store } from '@/store/store';
-import storeMock from './mockStore';
+import { act, render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+import { Provider } from 'react-redux';
+import { describe, expect, it } from 'vitest';
 import messages from '../../messages/en.json';
+import storeMock from './mockStore';
 
 const ARR_LENGTH = 2;
 const EMPTY_ARR = 0;
@@ -71,5 +71,50 @@ describe('RowEditor', () => {
     });
 
     expect(store.getState().rest.headers.length).toBe(EMPTY_ARR);
+  });
+
+  describe('RowEditor', () => {
+    it('should render correctly', () => {
+      render(
+        <Provider store={storeMock}>
+          <NextIntlClientProvider messages={messages} locale={locale}>
+            <table role='grid'>
+              <tfoot>
+                <RowEditor type='headers' />
+              </tfoot>
+            </table>
+          </NextIntlClientProvider>
+          ,
+        </Provider>,
+      );
+
+      expect(screen.getByTestId('row_editor')).toBeDefined();
+    });
+
+    it('change the state', () => {
+      render(
+        <Provider store={storeMock}>
+          <NextIntlClientProvider messages={messages} locale={locale}>
+            <table role='grid'>
+              <tfoot>
+                <RowEditor type='headers' />
+              </tfoot>
+            </table>
+          </NextIntlClientProvider>
+        </Provider>,
+      );
+
+      act(() => {
+        store.dispatch(headers(newHeaders));
+      });
+
+      expect(store.getState().rest.headers.length).toBe(ARR_LENGTH);
+
+      act(() => {
+        store.dispatch(headers([]));
+      });
+
+      expect(store.getState().rest.headers.length).toBe(EMPTY_ARR);
+    });
   });
 });
