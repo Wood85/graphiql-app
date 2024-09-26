9336e588c1fd3cbef24c9fb78f710097a073789d
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-26 16:21:32 +0300

    test: refactor the GraphiQLClient tests

--- src/__tests__/GraphiQLClient.test.tsx
+++ src/__tests__/GraphiQLClient.test.tsx
@@ -1,27 +1,18 @@
 import GraphiQLClient from '@/components/GraphiQLClient/GraphiQLClient';
-import { store } from '@/store/store';
 import '@testing-library/jest-dom';
-import { render, waitFor } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { waitFor } from '@testing-library/react';
+import { renderWithStore } from '../utils/testUtils';
 
 describe('GraphiQLClient', () => {
   it('should render correctly', () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <GraphiQLClient graphqlDocsIsOpen setIsDocsAvailable={() => {}} />
-      </Provider>,
-    );
+    const { getByText } = renderWithStore(<GraphiQLClient graphqlDocsIsOpen setIsDocsAvailable={() => {}} />);
 
     expect(getByText('Variables')).toBeInTheDocument();
     expect(getByText('Headers')).toBeInTheDocument();
   });
 
   it('display text', async () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <GraphiQLClient graphqlDocsIsOpen setIsDocsAvailable={() => {}} />
-      </Provider>,
-    );
+    const { getByText } = renderWithStore(<GraphiQLClient graphqlDocsIsOpen setIsDocsAvailable={() => {}} />);
 
     await waitFor(() => {
       expect(getByText('Loading...')).toBeInTheDocument();
