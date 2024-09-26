6ea24f5376c0b886f0018d04372188a113952919
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-26 16:13:34 +0300

    test: refactor the BodyEditor tests

--- src/__tests__/BodyEditor.test.tsx
+++ src/__tests__/BodyEditor.test.tsx
@@ -1,58 +1,29 @@
 import { BodyEditor } from '@/app/[locale]/components/RESTAPIClient/BodyEditor/BodyEditor';
-import { store } from '@/store/store';
-import { fireEvent, render } from '@testing-library/react';
-import { NextIntlClientProvider } from 'next-intl';
-import { Provider } from 'react-redux';
+import { fireEvent } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
-import messages from '../../messages/en.json';
-
-const locale = 'en';
+import { renderWithStore } from '../utils/testUtils';
 
 const ONE = 1;
 describe('BodyEditor component', () => {
   it('renders correctly', () => {
-    const { container } = render(
-      <Provider store={store}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <BodyEditor setBody={() => {}} />
-        </NextIntlClientProvider>
-      </Provider>,
-    );
+    const { container } = renderWithStore(<BodyEditor setBody={() => {}} />);
     expect(container).toMatchSnapshot();
   });
 
   it('renders headers editor by default', () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <BodyEditor setBody={() => {}} />
-        </NextIntlClientProvider>
-      </Provider>,
-    );
+    const { getByText } = renderWithStore(<BodyEditor setBody={() => {}} />);
     expect(getByText('Headers')).toBeDefined();
   });
 
   it('switches to body editor', () => {
-    const { getByText, getByRole } = render(
-      <Provider store={store}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <BodyEditor setBody={() => {}} />
-        </NextIntlClientProvider>
-      </Provider>,
-    );
+    const { getByText, getByRole } = renderWithStore(<BodyEditor setBody={() => {}} />);
     const bodyButton = getByRole('button', { name: 'Body' });
     fireEvent.click(bodyButton);
     expect(getByText('Body')).toBeDefined();
   });
 
   it('switches to variables editor', () => {
-    const { getByText, getByRole } = render(
-      <Provider store={store}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <BodyEditor setBody={() => {}} />
-        </NextIntlClientProvider>
-      </Provider>,
-    );
+    const { getByText, getByRole } = renderWithStore(<BodyEditor setBody={() => {}} />);
     const variablesButton = getByRole('button', { name: 'Variables' });
     fireEvent.click(variablesButton);
     expect(getByText('Variables')).toBeDefined();
