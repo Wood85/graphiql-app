2d4791d7e9e72685fcea93e6b6d6e38248d37600
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-26 16:28:39 +0300

    test: refactor the not-found tests

--- src/__tests__/not-found.test.tsx
+++ src/__tests__/not-found.test.tsx
@@ -1,19 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { NextIntlClientProvider } from 'next-intl';
 import NotFoundPage from '@/app/[locale]/not-found';
-import messages from '../../messages/en.json';
-
-const locale = 'en';
+import { renderWithIntl } from '../utils/testUtils';
 
 describe('NotFoundPage', () => {
   it('should render correctly', () => {
-    render(
-      <NextIntlClientProvider messages={messages} locale={locale}>
-        <NotFoundPage />
-      </NextIntlClientProvider>,
-    );
+    renderWithIntl(<NotFoundPage />);
 
     expect(screen.getByRole('img', { name: 'Not found' })).toBeDefined();
     expect(screen.getByRole('link').getAttribute('href')).toBe('/');
