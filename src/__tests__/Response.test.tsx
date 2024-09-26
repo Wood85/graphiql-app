60a63b1c0b56b5565de0892f25e4539ab63f3b70
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-26 16:39:02 +0300

    refactor: remove unused variable

--- src/__tests__/Response.test.tsx
+++ src/__tests__/Response.test.tsx
@@ -2,7 +2,6 @@ import { Response, type IProps } from '@/components/Response/Response';
 import { TRequestMethod } from '@/interfaces/RequestMethod';
 import { renderWithStore } from '../utils/testUtils';
 
-const locale = 'en';
 describe('Response component', () => {
   const props: IProps = {
     method: TRequestMethod.GET,
