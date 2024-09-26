ff541da43c864181063eb18bb143a05e8b36ed79
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-26 16:27:35 +0300

    test: refactor the HeadersEditor tests

--- src/__tests__/HeadersEditor.test.tsx
+++ src/__tests__/HeadersEditor.test.tsx
@@ -1,17 +1,12 @@
 import { HeadersEditor } from '@/components/GraphiQLClient/HeadersEditor/HeadersEditor';
 import { gqlHeaders } from '@/store/reducers/graphqlSlice';
 import { store } from '@/store/store';
-import { act, render, screen } from '@testing-library/react';
-import { NextIntlClientProvider } from 'next-intl';
-import { Provider } from 'react-redux';
+import { act, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
-import messages from '../../messages/en.json';
-import storeMock from './mockStore';
+import { renderWithStore } from '../utils/testUtils';
 
 const ARR_LENGTH = 2;
 const EMPTY_ARR = 0;
-
-const locale = 'en';
 
 const newHeaders = [
   {
@@ -30,27 +25,13 @@ const newHeaders = [
 
 describe('HeadersEditor', () => {
   test('should render HeadersEditor component', () => {
-    render(
-      <Provider store={storeMock}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <HeadersEditor />
-        </NextIntlClientProvider>
-        ,
-      </Provider>,
-    );
+    renderWithStore(<HeadersEditor />);
 
     expect(screen.getByTestId('headers_editor_gql')).toBeDefined();
   });
 
   test('change the state when dispatching "headers"', () => {
-    render(
-      <Provider store={storeMock}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <HeadersEditor />
-        </NextIntlClientProvider>
-        ,
-      </Provider>,
-    );
+    renderWithStore(<HeadersEditor />);
 
     act(() => {
       store.dispatch(gqlHeaders(newHeaders));
