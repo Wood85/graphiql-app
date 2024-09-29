79508c942798237379694d4cb9329280c87c526b
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-09-29 20:50:20 +0300

    fix: use the correct messages file in testUtils

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -4,22 +4,29 @@ import { NextIntlClientProvider } from 'next-intl';
 import { Provider } from 'react-redux';
 import storeMock from '../__tests__/mockStore';
 
-import messages from '../../messages/en.json';
+import messages_en from '../../messages/en.json';
+import messages_ru from '../../messages/ru.json';
 
-const renderWithIntl = (children: React.ReactNode, locale: 'en' | 'ru' = 'en'): RenderResult =>
-  render(
+const renderWithIntl = (children: React.ReactNode, locale: 'en' | 'ru' = 'en'): RenderResult => {
+  const messages = locale === 'en' ? messages_en : messages_ru;
+
+  return render(
     <NextIntlClientProvider messages={messages} locale={locale}>
       {children}
     </NextIntlClientProvider>,
   );
+};
 
-const renderWithStore = (children: React.ReactNode, locale: 'en' | 'ru' = 'en'): RenderResult =>
-  render(
+const renderWithStore = (children: React.ReactNode, locale: 'en' | 'ru' = 'en'): RenderResult => {
+  const messages = locale === 'en' ? messages_en : messages_ru;
+
+  return render(
     <Provider store={storeMock}>
       <NextIntlClientProvider messages={messages} locale={locale}>
         {children}
       </NextIntlClientProvider>
     </Provider>,
   );
+};
 
 export { renderWithIntl, renderWithStore };
