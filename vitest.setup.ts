c1ff50d2a003d6dc39febc805727e08c36635573
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-10-01 23:10:15 +0300

    feat: configure msw

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,18 @@
 import { vi } from 'vitest';
 import '@testing-library/jest-dom';
+import { server } from '@/tests/setup/msw/server';
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
 
 vi.mock('@/firebase/firebase.ts', () => ({
   app: {},
