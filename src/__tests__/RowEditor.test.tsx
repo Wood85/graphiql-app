b8714c5962d6d3bcabfca9137cc676306d6a56dc
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-10-02 12:25:07 +0300

    test: complement the RowEditor component tests

--- src/__tests__/RowEditor.test.tsx
+++ src/__tests__/RowEditor.test.tsx
@@ -1,7 +1,8 @@
 import RowEditor from '@/app/[locale]/components/RowEditor/RowEditor';
-import { headers } from '@/store/reducers/restFullSlice';
+import { headers, variables } from '@/store/reducers/restFullSlice';
+import { gqlHeaders } from '@/store/reducers/graphqlSlice';
 import { store } from '@/store/store';
-import { act, screen } from '@testing-library/react';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
 import { renderWithStore } from '@/utils/testUtils';
 
 const ARR_LENGTH = 2;
@@ -17,6 +18,23 @@ const newHeaders = [
   {
     key: 'Accept',
     value: '*/*',
+    checked: true,
+    userDefined: true,
+  },
+];
+
+const newGqlHeaders = [...newHeaders];
+
+const newVariables = [
+  {
+    key: 'my_variable_1',
+    value: 'my_value_1',
+    checked: true,
+    userDefined: true,
+  },
+  {
+    key: 'my_variable_2',
+    value: 'my_value_2',
     checked: true,
     userDefined: true,
   },
@@ -57,39 +75,47 @@ describe('RowEditor', () => {
     expect(store.getState().rest.headers.length).toBe(EMPTY_ARR);
   });
 
-  describe('RowEditor', () => {
-    it('should render correctly', () => {
-      renderWithStore(
-        <table role='grid'>
-          <tfoot>
-            <RowEditor type='headers' />
-          </tfoot>
-        </table>,
-      );
+  it('change the state when dispatching "variables" ', () => {
+    renderWithStore(
+      <table role='grid'>
+        <tfoot>
+          <RowEditor type='variables' />
+        </tfoot>
+      </table>,
+    );
 
-      expect(screen.getByTestId('row_editor')).toBeDefined();
+    act(() => {
+      store.dispatch(variables(newVariables));
     });
 
-    it('change the state', () => {
-      renderWithStore(
-        <table role='grid'>
-          <tfoot>
-            <RowEditor type='headers' />
-          </tfoot>
-        </table>,
-      );
+    expect(store.getState().rest.variables.length).toBe(ARR_LENGTH);
 
-      act(() => {
-        store.dispatch(headers(newHeaders));
-      });
-
-      expect(store.getState().rest.headers.length).toBe(ARR_LENGTH);
-
-      act(() => {
-        store.dispatch(headers([]));
-      });
-
-      expect(store.getState().rest.headers.length).toBe(EMPTY_ARR);
+    act(() => {
+      store.dispatch(variables([]));
     });
+
+    expect(store.getState().rest.variables.length).toBe(EMPTY_ARR);
+  });
+
+  it('change the state when dispatching "gqlHeaders" ', () => {
+    renderWithStore(
+      <table role='grid'>
+        <tfoot>
+          <RowEditor type='graphqlHeaders' />
+        </tfoot>
+      </table>,
+    );
+
+    act(() => {
+      store.dispatch(gqlHeaders(newGqlHeaders));
+    });
+
+    expect(store.getState().graphql.headers.length).toBe(ARR_LENGTH);
+
+    act(() => {
+      store.dispatch(gqlHeaders([]));
+    });
+
+    expect(store.getState().graphql.headers.length).toBe(EMPTY_ARR);
   });
 });
